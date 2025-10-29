74177c3670e3898be6cfc123cef46739bab3bbb2
Nam Trần Đăng <dangnamtran.dev@gmail.com>
2025-10-29 15:44:17 +0700

    feat: add train utils - multi-GPU mode, seed mode for consistent execute YOLO11 model

---
+++ utils/YOLO11_Train_Utils.py
@@ -0,0 +1,48 @@
+import random
+import numpy as np
+import torch
+
+''' Store train utils - setup training '''
+def setup_seed():
+    random.seed(0)
+    np.random.seed(0)
+    torch.manual_seed(0)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+# b. Multi-processing and multi-threading
+def setup_multi_processes():
+    import cv2
+    from os import environ
+    from platform import system
+
+    # set multiprocess start method as `fork` to speed up the training
+    if system() != 'Windows':
+        torch.multiprocessing.set_start_method('fork', force=True)
+
+    # disable opencv multithreading to avoid system being overloaded
+    cv2.setNumThreads(0)
+
+    # setup OMP threads
+    if 'OMP_NUM_THREADS' not in environ:
+        environ['OMP_NUM_THREADS'] = '1'
+
+    # setup MKL threads
+    if 'MKL_NUM_THREADS' not in environ:
+        environ['MKL_NUM_THREADS'] = '1'
+
+
+# c. Anchor Points (Not anchor like tradition) ---> Anchor free mechanism 
+def make_anchors(x, strides, offset=0.5):
+    assert x is not None
+    anchor_tensor, stride_tensor = [], []
+    dtype, device = x[0].dtype, x[0].device
+    for i, stride in enumerate(strides):
+        _, _, h, w = x[i].shape
+        sx = torch.arange(end=w, device=device, dtype=dtype) + offset  # shift x
+        sy = torch.arange(end=h, device=device, dtype=dtype) + offset  # shift y
+        sy, sx = torch.meshgrid(sy, sx)
+        anchor_tensor.append(torch.stack((sx, sy), -1).view(-1, 2))
+        stride_tensor.append(torch.full((h * w, 1), stride, dtype=dtype, device=device))
+    return torch.cat(anchor_tensor), torch.cat(stride_tensor)
+
